546b98aa1d3328642a4230f083338e0befd4a62f
Arda Sozer <asozer@berkeley.edu>
2019-10-02 13:53:12 -0700

    finished part 2

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,10 +7,16 @@ class HangpersonGame
 
   # def initialize()
   # end
+
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+
+
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -23,5 +29,53 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
+
+  def guess(letter)
+    if letter == nil
+      raise ArgumentError, 'Must not be Nil'
+    elsif not letter =~ /[[:alpha:]]/
+      raise ArgumentError, 'Must be alphabetic'
+    elsif letter == ''
+      raise ArgumentError, 'Empty String'
+    elsif @word.include?(letter.downcase)
+      if not @guesses.include?(letter.downcase)
+        @guesses << letter.downcase
+      else 
+        return false
+      end
+    elsif not @wrong_guesses.include?(letter.downcase)
+      @wrong_guesses << letter.downcase
+    else 
+      return false
+    end
+  end
+
+  def word_with_guesses
+    if @guesses.length() > 0
+      @word.gsub(/[^#{@guesses}]/, '-')
+    else
+      @word.gsub(/./, '-')
+    end 
+  end
+
+  def guess_several_letters(game, letters)
+    for letter in letters
+      game.guess(letter)
+    end
+  end
+
+
+
+  def check_win_or_lose
+    if @wrong_guesses.length() >= 7
+      return :lose
+    elsif @guesses.length() == @word.length()
+      return :win
+    else 
+      return :play
+    end
+  end 
+
+
 
 end
